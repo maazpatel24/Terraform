5afd48338a7778fb57928cf15aa9c6938e7ff2ed
Maaz Patel <maazptl240602@gmail.com>
2024-02-15 14:39:44 +0530

    Resource Dependencies basics

---
+++ Resource Dependencies/key-generator/key.tf
@@ -0,0 +1,9 @@
+resource "local_file" "key_details" {
+  filename = "/root/key.txt"
+  content = tls_private_key.pvtkey.private_key_pem
+}
+
+resource "tls_private_key" "pvtkey" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
